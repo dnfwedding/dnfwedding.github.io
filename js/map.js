cf29d8b185a2c4c32d3d9fbb144787df0a875a6a
dnfwedding <fabiola.pina.marquez@gmail.com>
2017-10-25 20:38:42 +0300

    Switch to yandex maps + delete BBQ imgs

--- js/map.js
+++ js/map.js
@@ -1,12 +1,18 @@
-(function googleMap() {
-    var mapOptions = {
-        center: {lat: 59.9338318, lng: 30.294362},
-        zoom: 17,
-        mapTypeId: google.maps.MapTypeId.MAP
+(function yandexMap() {
+    console.log('Test 0');
+    ymaps.ready(init);
+    var yMap;
+
+    function init(){
+        console.log('Test 1');
+        yMap = new ymaps.Map("map-panel", {
+            center: [59.9338318, 30.294362],
+            zoom: 16
+        });
+        var mark = new ymaps.Placemark(
+            [59.934148, 30.294086],
+            {hintContent: 'ZAGS #1', balloonContent: 'Registration will be here! ^_^'}
+        );
+        yMap.geoObjects.add(mark);
     }
-    var map = new google.maps.Map(document.getElementById("map-panel"), mapOptions);
-    var marker = new google.maps.Marker({
-        position: {lat: 59.934148, lng: 30.294086},
-        map: map
-    });
 })();
